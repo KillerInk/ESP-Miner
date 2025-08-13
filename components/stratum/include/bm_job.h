b58e5911391e03de220ebae728578f68b50f96ec
killerink <killerink@email.de>
2025-08-13 15:33:26 +0200

    move h only files into include

---
+++ components/stratum/include/bm_job.h
@@ -0,0 +1,25 @@
+#ifndef BM_JOB_H_
+#define BM_JOB_H_
+
+typedef struct
+{
+    uint32_t version;
+    uint32_t version_mask;
+    uint8_t prev_block_hash[32];
+    uint8_t prev_block_hash_be[32];
+    uint8_t merkle_root[32];
+    uint8_t merkle_root_be[32];
+    uint32_t ntime;
+    uint32_t target; // aka difficulty, aka nbits
+    uint32_t starting_nonce;
+
+    uint8_t num_midstates;
+    uint8_t midstate[32];
+    uint8_t midstate1[32];
+    uint8_t midstate2[32];
+    uint8_t midstate3[32];
+    uint32_t pool_diff;
+    char *jobid;
+    char *extranonce2;
+} bm_job;
+#endif
--- components/stratum/include/mining.h
+++ components/stratum/include/mining.h
@@ -2,28 +2,8 @@
 #define MINING_H_
 
 #include "stratum_api.h"
+#include "bm_job.h"
 
-typedef struct
-{
-    uint32_t version;
-    uint32_t version_mask;
-    uint8_t prev_block_hash[32];
-    uint8_t prev_block_hash_be[32];
-    uint8_t merkle_root[32];
-    uint8_t merkle_root_be[32];
-    uint32_t ntime;
-    uint32_t target; // aka difficulty, aka nbits
-    uint32_t starting_nonce;
-
-    uint8_t num_midstates;
-    uint8_t midstate[32];
-    uint8_t midstate1[32];
-    uint8_t midstate2[32];
-    uint8_t midstate3[32];
-    uint32_t pool_diff;
-    char *jobid;
-    char *extranonce2;
-} bm_job;
 
 void free_bm_job(bm_job *job);
 
