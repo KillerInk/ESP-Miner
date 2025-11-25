ebd4952e0ef69dc256abf3eb5ce71c3a2d617735
killerink <killerink@email.de>
2025-11-25 18:12:16 +0100

    let bm1370 roll

--- components/asic/bm1370.c
+++ components/asic/bm1370.c
@@ -158,6 +158,103 @@ void BM1370_send_hash_frequency(float target_freq)
     ESP_LOGI(TAG, "Setting Frequency to %g MHz (%g)", target_freq, frequency);
 }
 
+void BM1370_set_hash_counting_number(int hcn) {
+    uint8_t set_10_hash_counting[6] = {0x00, 0x10, 0x00, 0x00, 0x00, 0x00};
+    set_10_hash_counting[2] = (hcn >> 24) & 0xFF;
+    set_10_hash_counting[3] = (hcn >> 16) & 0xFF;
+    set_10_hash_counting[4] = (hcn >> 8) & 0xFF;
+    set_10_hash_counting[5] = hcn & 0xFF;
+    _send_BM1370((TYPE_CMD | GROUP_ALL | CMD_WRITE), set_10_hash_counting, 6, BM1370_SERIALTX_DEBUG);
+}
+
+static void BM1370_set_chip_nonce_offset(int chain_chip_count, float cno_interval) {
+    // CNO: Dividing by chain 
+    int address_interval = 256/_largest_power_of_two(chain_chip_count);   
+
+    ESP_LOGI(TAG, "Chip address interval=%i", address_interval);
+
+    int cno_chip_value = 0;
+    unsigned char set_0C_chip_nonce_offset[6] = {0x00,0x0C, 0x00, 0x00, 0x00, 0x00};
+    for (uint8_t chip_idx = 0; chip_idx < chain_chip_count; chip_idx++) {
+
+        if (chip_idx > 0) cno_chip_value = (int)(cno_interval * (float)chip_idx) + 1;
+        set_0C_chip_nonce_offset[0] = chip_idx * address_interval;
+        set_0C_chip_nonce_offset[2] = 0x80; //CNOV,  CNO valid flag only use cno if this bit is on
+        set_0C_chip_nonce_offset[3] = 0x00; // reserved
+        set_0C_chip_nonce_offset[4] = (cno_chip_value >> 8) & 0xFF;
+        set_0C_chip_nonce_offset[5] = cno_chip_value & 0xFF;
+        _send_BM1370((TYPE_CMD | GROUP_SINGLE | CMD_WRITE), set_0C_chip_nonce_offset, 6, BM1370_SERIALTX_DEBUG);
+    }
+
+    ESP_LOGI(TAG, "Chip setting cno_interval=%f",cno_interval);
+}
+
+static float calculate_cno_interval(int chips) {
+    // Register CNO Chip Nonce Offset
+    //          
+    // is a optional nonce offset for bm1370 for chips in a chain
+    // it causes the chip to start at a paritcular location byte1
+    // It has more precision than address interval due to more bits being available
+    // Note: CNO overrides the size set by address interval
+
+    // address interval = 0x100 / _largest_power_of_two(chips) << 8 (8 bit precision)
+    // cno              = 0x10000 / _largest_power_of_two(chips)    (16 bit precision)
+
+    int cno_interval_max = 0x10000;
+
+    // a float is kept as it will be multiplied by the chip address when sending the commands to chips
+    float cno_interval = (float)(cno_interval_max / (float)chips);
+    return cno_interval;
+}
+
+static float calculate_fully_reserved_space(int big_cores, int chain_chip_count, float cno_interval)
+{
+    int address_interval = 256/_largest_power_of_two(chain_chip_count);
+    // Nonce Size for a particular setting
+    // 
+    // Calculates the  size of the distrobuted space
+    // This function enables the time and the hcn register to be set properly 
+    big_cores = _largest_power_of_two(big_cores);
+
+    uint32_t max_nonce_range = 0xffffffff;
+    uint32_t chain_reserve = 0x100;
+    uint32_t chain_reserve16 = chain_reserve << 8;
+    uint32_t address_interval16 = address_interval << 8;
+    uint32_t fully_divided_space = max_nonce_range / (uint32_t)big_cores / chain_reserve16;
+
+    // address interval calculation
+    float fully_reserved_space = (float)fully_divided_space * (float)address_interval16;
+
+    // cno_interval overrides address interval calc
+    if (cno_interval > 0) fully_reserved_space = fully_divided_space * cno_interval;
+
+    return fully_reserved_space;
+}
+# define XTAL_OSC_MHZ 25
+
+int calculate_version_rolling_hcn(int big_cores, int chain_chip_count, int frequency, float cno_interval) {
+    // Register HCN Hash Counting Number
+    //          
+    // Calulates the nonce size for version rolling chips
+    // It signifies to the chip when generate the next version and restart the nonce range
+    // This function ensures HCN does not cause duplicates
+    // Warning: HCN can cause duplicates if set too large if you decide not to use this function.
+    float fully_reserved_space = calculate_fully_reserved_space(big_cores, chain_chip_count, cno_interval);
+    int hcn = (fully_reserved_space * ((float)XTAL_OSC_MHZ / (float)frequency) / 2.0);
+    ESP_LOGI(TAG, "Chip setting freq=%i chain_chip_count=%i size=%f", frequency, chain_chip_count, fully_reserved_space);
+    return hcn;
+}
+
+void BM1370_set_nonce_percent(uint64_t frequency, uint16_t chain_chip_count) {
+    float cno_interval = calculate_cno_interval(chain_chip_count);
+    BM1370_set_chip_nonce_offset(chain_chip_count, cno_interval);
+
+    int hcn = calculate_version_rolling_hcn(ASIC_BM1370.core_count, chain_chip_count, (int)1200, 0);
+    BM1370_set_hash_counting_number(hcn);
+
+    ESP_LOGI(TAG, "Chip setting chips=%i freq=%i hcn=%i chain_chip_count=%i", chain_chip_count, (int)frequency, hcn, chain_chip_count);
+}
+
 uint8_t BM1370_init(float frequency, uint16_t asic_count, uint16_t difficulty)
 {
     // set version mask
@@ -254,16 +351,18 @@ uint8_t BM1370_init(float frequency, uint16_t asic_count, uint16_t difficulty)
 
     //ramp up the hash frequency
     do_frequency_transition(frequency, BM1370_send_hash_frequency);
+    BM1370_set_nonce_percent(frequency, chip_counter);
 
+    BM1370_set_version_mask(STRATUM_DEFAULT_VERSION_MASK);
     //register 10 is still a bit of a mystery. discussion: https://github.com/bitaxeorg/ESP-Miner/pull/167
 
     // unsigned char set_10_hash_counting[6] = {0x00, 0x10, 0x00, 0x00, 0x11, 0x5A}; //S19k Pro Default
     // unsigned char set_10_hash_counting[6] = {0x00, 0x10, 0x00, 0x00, 0x14, 0x46}; //S19XP-Luxos Default
     // unsigned char set_10_hash_counting[6] = {0x00, 0x10, 0x00, 0x00, 0x15, 0x1C}; //S19XP-Stock Default
     //unsigned char set_10_hash_counting[6] = {0x00, 0x10, 0x00, 0x00, 0x15, 0xA4}; //S21-Stock Default
-    unsigned char set_10_hash_counting[6] = {0x00, 0x10, 0x00, 0x00, 0x1E, 0xB5}; //S21 Pro-Stock Default
+    //unsigned char set_10_hash_counting[6] = {0x00, 0x10, 0x00, 0x00, 0x1E, 0xB5}; //S21 Pro-Stock Default
     // unsigned char set_10_hash_counting[6] = {0x00, 0x10, 0x00, 0x0F, 0x00, 0x00}; //supposedly the "full" 32bit nonce range
-    _send_BM1370((TYPE_CMD | GROUP_ALL | CMD_WRITE), set_10_hash_counting, 6, BM1370_SERIALTX_DEBUG);
+    //_send_BM1370((TYPE_CMD | GROUP_ALL | CMD_WRITE), set_10_hash_counting, 6, BM1370_SERIALTX_DEBUG);
 
     return chip_counter;
 }
